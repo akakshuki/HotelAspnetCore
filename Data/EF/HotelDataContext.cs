3411566e09fca6b7961294bed8b6a2d82a1e87b5
akakshuki <giangbaccai1207@gmail.com>
2020-02-23 01:28:30 +0700

    database design

---
+++ Data/EF/HotelDataContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.EF
+{
+    public class HotelDataContext : DbContext
+    {
+        public HotelDataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ Data/EF/HotelDataContextFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Data.EF
+{
+    public class HotelDataContextFactory : IDesignTimeDbContextFactory<HotelDataContext>
+    {
+        //setting data context 
+        public HotelDataContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsetting.json")
+                .Build();
+
+
+            var connectionString = configuration.GetConnectionString("HotelDB");
+
+
+            var optionsBuilder = new DbContextOptionsBuilder<HotelDataContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new HotelDataContext(optionsBuilder.Options);
+
+        }
+    }
+}
